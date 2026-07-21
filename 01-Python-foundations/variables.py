996c20f0543454f03c729eefd27240e4fe973ffd
shashank <u10shashank@gmail.com>
2026-07-21 20:37:21 +0530

    adding the variables files

---
+++ 01-Python-foundations/variables.py
@@ -0,0 +1,32 @@
+# Variables, data types and operators.
+
+a = 20 # where a is variables and 10 is constant
+
+b = 10 # 20 is a integer data types
+
+c = 1.5 # this is a float data types
+
+d = False # this is boolean data types
+
+e = "shashank" # this is a string data types
+
+print(a , b , c , d , e)
+
+user_input = input(" enter a number : ") # taking user input
+
+print(user_input)
+
+print(type(user_input)) # print data types of the variables
+
+user_input1 = int(input(" enter a number : ")) # type casting #you can only enter a int only
+
+print(user_input1)
+
+print(type(user_input1))
+
+# operators
+
+print("addition of a and b : " , a + b)
+print("subtraction of a and b : " , a - b)
+print("multiplication of a and b : " , a * b)
+print("division of a and b : " , a / b)
