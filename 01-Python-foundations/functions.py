efb729508c8d4feb8b752a2f73f9a45ec0b38d2e
shashank <u10shashank@gmail.com>
2026-07-21 21:08:35 +0530

    adding new files

--- 01-Python-foundations/functions.py
+++ 01-Python-foundations/functions.py
@@ -1,6 +1,6 @@
 # functions
 
-def user_input():
+def user_input(env):
     if env == "dev" or env == "stg":
         print("you can deploy on friday")
     elif env == "prd":
@@ -8,5 +8,5 @@ def user_input():
     else:
         print(" you can deploy on any day")    
 
-env = input("enter the environment : ")
-user_input(env)
+# env = input("enter the environment : ")
+# user_input(env) 
---
+++ 01-Python-foundations/loops.py
@@ -0,0 +1,14 @@
+from functions import user_input
+
+# while loop
+
+num = 1
+while num <= 5:
+    print(num)
+    num = num + 1
+
+# for loop
+
+for i in range(5):
+    env = input("Enter the env : ")
+    user_input(env)
